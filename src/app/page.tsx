79a9ea891026e90155d45c120ba9267f293b5649
SnailPastes <quacky888@gmail.com>
2025-07-10 14:17:34 +0700

    Added prisma and connected it to vercel

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home(){
+  return(
+    <section className="py-24">
+      <div>
+        <h1 className="text-3xl font-bold">Noted start</h1>
+      </div>
+    </section>
+  )
+}
